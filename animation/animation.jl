240cb5f9247e45297964676b99326c2c48ef1623
sergio.egm <s.e.g.manfrin@gmail.com>
2025-05-28 16:22:32 +0200

    Updated animation.jl

--- animation/animation.jl
+++ animation/animation.jl
@@ -1,13 +1,17 @@
 using Images , FileIO , Printf
 
-function make_gif(folder::String , size::Int , gens ::Int , fileName::String)
+function make_gif(folder::String , size::Int , gens::Int , gen0::Int , fileName::String)
     frames = []
 
     count = 0
 
+    println("Size: " , size)
+    println("# of generations: " , gens)
+    println("Initial generation: " , gen0)
+
     print("[")
 
-    for i in 0:gens
+    for i in gen0:gens
         path = joinpath(folder , @sprintf("frame%d.bin" , i))
         raw  = read(path)
 
@@ -29,19 +33,21 @@ end
 
 function main()
     # Get the size of the lactice and the number of generations
-    gens , size = open("INPUT.dat") do io
+    gens , size , gen0 = open("INPUT.dat") do io
         init_gen = parse(Bool , readline(io))
         x = parse(Int , readline(io))
         y = -1
+        z = 0
         if init_gen
             file_name = "save/config.dat"
             if !isfile(file_name)
                 file_name = ".bckup/initial.dat"
             end
             appo = open(file_name) do fin
-                return readline(fin)
+                return split(readline(fin))
             end
-            y = parse(Int , split(appo)[1])
+            y = parse(Int , appo[1])
+            z = parse(Int , appo[3])
         else
             readline(io)
             y = parse(Int , readline(io))
@@ -49,10 +55,12 @@ function main()
 
         @assert (y > 0) "Not valid size"
 
-        return x , y
+        x += z
+
+        return x , y , z
     end
 
-    make_gif("data" , size , gens , "life.gif")
+    make_gif("data" , size , gens , gen0 ,  "life.gif")
 end
 
 main()
